c0efd6cefa59843d97966298857b47aa0d2a384e
Ahmed Sherif <36515991+ahmdshrf@users.noreply.github.com>
2019-07-04 01:34:28 +0200

    Editing Graph Class

--- Graphs.cpp
+++ Graphs.cpp
@@ -1,89 +1,23 @@
 #include <iostream>
-#include <vector>
+#include <vector> 
 #include <list>
-#include <stack>
-#include <queue>
+#include <queue> 
+
 using namespace std;
-
-
-/*int v = 1000;
-int e = 20000;
-
-vector <vector <int>> adj_mat(v, vector<int>(v));
-*/
-
-
-
-/*void dfs(vector < list<int>> & adjacency_list, vector<bool>visited, int source)
+class Graph
 {
-	visited[source] = true;
-	for (auto itr = adjacency_list[source].begin(); itr != adjacency_list[source].end(); itr++)
-	{
-		if (!visited[*itr])
-			dfs(adjacency_list, visited, *itr);
+	int nodes_no;
+	list<pair<int, int>> *nodes;
+
+public:
+
+	Graph(int n) {
+		this->nodes_no = n;
+		nodes = new list<pair<int, int>>[n];
 	}
-}*/
 
-void dfs(vector<list<int>>& adjacency_list, int source)
-{
-	stack <int> to_be_visited;
-	vector<bool> visited(adjacency_list.size());
-	to_be_visited.push(source);
-	visited[source] = true;
-	while (!to_be_visited.empty())
-	{
-		int current_node = to_be_visited.top();
-		to_be_visited.pop();
-		for (auto itr = adjacency_list[current_node].begin(); itr !=
-			adjacency_list[current_node].end(); itr++)
-		{
-			if (!visited[*itr])
-			{
-				to_be_visited.push(*itr);
-				visited[*itr] = true;
-			}
-		}
+	void add_node(int from, int to, int weight) {
+		nodes[from].push_back(make_pair(to, weight));
+		nodes[to].push_back(make_pair(from, weight));
 	}
-	cout << to_be_visited.top() << " ";
-}
-void bfs(vector<list<int>>& adjacency_list, int source)
-{
-	queue<int> to_be_visited;
-	vector<bool> visited(adjacency_list.size());
-	to_be_visited.push(source);
-	visited[source] = true;
-	while (!to_be_visited.empty())
-	{
-		int current_node = to_be_visited.front();
-		to_be_visited.pop();
-		for (auto itr = adjacency_list[current_node].begin();
-			itr != adjacency_list[current_node].end(); itr++)
-		{
-			if (!visited[*itr])
-			{
-				to_be_visited.push(*itr);
-				visited[*itr] = true;
-			}
-		}
-	}
-}
-int main()
-{
-	vector <list <int>> adj_list(5);
-	adj_list[0].push_back(1);
-	adj_list[0].push_back(2);
-	adj_list[0].push_back(3);
-	adj_list[1].push_back(0);
-	adj_list[1].push_back(2);
-	adj_list[2].push_back(3);
-	adj_list[2].push_back(0);
-	adj_list[2].push_back(1);
-	adj_list[2].push_back(4);
-	adj_list[3].push_back(0);
-	adj_list[3].push_back(2);
-	adj_list[3].push_back(4);
-	adj_list[4].push_back(3);
-	adj_list[4].push_back(2);
-	dfs(adj_list, 1);
-}
-
+};
